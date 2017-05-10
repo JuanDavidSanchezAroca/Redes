ebb6bce0eedd0cd5ee0355954f986c093bc696ec
cardonacarlos9a <cardonacarlos9a@gmail.com>
2017-05-09 22:17:48 -0500

    Update Nmap.java

--- ProyectoRedesNmap/src/com/uniquindio/Nmap.java
+++ ProyectoRedesNmap/src/com/uniquindio/Nmap.java
@@ -248,5 +248,92 @@ public class Nmap {
 	public static void setInterfacesLista(List<String> interfacesLista) {
 		Nmap.interfacesLista = interfacesLista;
 	}
+	
+	/**
+	package nmap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetPermission;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		  final ExecutorService es = Executors.newFixedThreadPool(65000);
+		  final String ip = "192.168.1.254";
+		  final int timeout = 400;
+		  final List<Future<Boolean>> futures = new ArrayList<>();
+		  for (int port = 1; port <= 65535; port++) {
+		    futures.add(portIsOpen(es, ip, port, timeout));
+		  }
+		  es.shutdown();
+		  int openPorts = 0;
+		  int number=1;
+		  for (final Future<Boolean> f : futures) {
+		    try {
+				if (f.get()) {
+					System.out.println(number+ " " +f.get());
+				  openPorts++;
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				continue;
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				continue;
+			}
+		    //System.out.println(number+" "+f.get());
+		    number++;
+		    
+		  }
+		  System.out.println("There are " + openPorts + " open ports on host " + ip + " (probed with a timeout of " + timeout + "ms)");
+
+
+	}
+// 
+	public static Future<Boolean> portIsOpen(final ExecutorService es, final String ip, final int port, final int timeout) {
+		  return es.submit(new Callable<Boolean>() {
+		
+		      @Override public Boolean call() {
+		        try {
+		          Socket socket = new Socket();
+		          socket.connect(new InetSocketAddress(ip, port), timeout);
+		          socket.close();
+		          
+		          return true;
+		        } catch (Exception ex) {
+		          return false;
+		        }
+		      }
+		   });
+		}
+	
+	
+
+}
+	*/
 
 }
